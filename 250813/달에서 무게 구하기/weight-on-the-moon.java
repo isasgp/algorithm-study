828a6653b85b8cdab4f6ccc65de0235c6781d74b
Park Do Hoon <139849985+isasgp@users.noreply.github.com>
2025-08-13 17:23:39 +0900

    Title: 달에서 무게 구하기, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250813/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int m = 13;
+        double rate = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f\n", m, rate, m*rate);
+    }
+}
