067b98b889c0fb4f2bd17ff2627a3fc0e05cf216
Park Do Hoon <139849985+isasgp@users.noreply.github.com>
2025-08-13 20:12:39 +0900

    Title: 함수를 이용한 소수 판별, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250813/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrimeNum(int num) {
+        int numSqrt = (int)Math.sqrt(num) + 1;
+        for(int i=2; i<= numSqrt; i++) {
+            if(num%i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static int sumPrimeNum(int a, int b) {
+        int sum = 0;
+        for(int i=a; i<=b; i++) {
+            if(isPrimeNum(i))
+                sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(sumPrimeNum(a, b));
+    }
+}
