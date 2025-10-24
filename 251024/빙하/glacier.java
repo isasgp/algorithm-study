45fd636ccbd44dfcbfe7b5eb6327df11d5f8362d
Park Do Hoon <139849985+isasgp@users.noreply.github.com>
2025-10-24 17:53:53 +0900

    Title: 빙하, Time: 849ms, Memory: 53MB, Status: Passed - Codetree

--- 251024/빙하/glacier.java
+++ 251024/빙하/glacier.java
@@ -46,48 +46,6 @@ public class Main {
         return true;
     }
 
-    private static boolean isSurrounded(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
-        boolean[][] isVisited = new boolean[MAX_NUM+1][MAX_NUM+1];
-
-        queue.add(new Point(x, y));
-        isVisited[x][y] = true;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        while(!queue.isEmpty()) {
-            Point p = queue.poll();
-            int currX = p.x, currY = p.y;
-            for(int dir=0; dir<4; dir++) {
-                int newX = currX + dx[dir];
-                int newY = currY + dy[dir];
-
-                if(!inRange(newX, newY)) 
-                    return false;
-                
-                if((!isVisited[newX][newY]) && grid[newX][newY] == 0) {
-                    isVisited[newX][newY] = true;
-                    queue.add(new Point(newX, newY));
-                }
-            }
-        }
-        return true;
-    }
-
-    private static boolean canStart(int x, int y) {
-        if(grid[x][y] == 1)
-            return false;
-        
-        if(visited[x][y])
-            return false;
-
-        if(isSurrounded(x, y))
-            return false;
-
-        return true;
-    }
-
     private static void solution() {
         int[][] newGrid = new int[n][m];
         for(int i=0; i<n; i++) {
@@ -131,22 +89,17 @@ public class Main {
         int time = 0;
         int beforeCnt = 0;
 
-        for(int t=1; t<10; t++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if(canStart(i, j)) {
-                        q.add(new Point(i, j));
-                        visited[i][j] = true;
-                    }
-                }
-            }
+        while(true) {
+            time++;
 
             beforeCnt = countGlacier();
+
+            q.add(new Point(0, 0));
+            visited[0][0] = true;
 
             solution();
 
             if(countGlacier() == 0) {
-                time = t;
                 break;
             }
 
