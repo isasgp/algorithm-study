49b8f198618cd23fce7e3f812460f185816412ba
Park Do Hoon <139849985+isasgp@users.noreply.github.com>
2025-10-26 00:52:43 +0900

    Title: 서로 다른 BST 개수 세기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251026/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 251026/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 public class Main {
     private static final int MAX_NUM = 19;
+    private static long[] dp = new long[MAX_NUM+1];
 
     private static long solution(int n) {
-        long[] dp = new long[MAX_NUM+1];
-
+        dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 5;
-        for(int i=4; i<=n; i++) {
-            dp[i] = 2*dp[i-1] + 2*dp[i-2] + 2*dp[i-3];
+
+        if(dp[n] != 0)
+            return dp[n];
+        
+        long temp = 0;
+
+        for(int i=0; i<n; i++) {
+            temp += solution(i) * solution(n - i - 1);
         }
+        dp[n] = temp;
+
         return dp[n];
     }
 
