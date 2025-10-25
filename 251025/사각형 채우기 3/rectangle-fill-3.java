84baf4e526c7c387b45f854ab9d3faa835807fb2
Park Do Hoon <139849985+isasgp@users.noreply.github.com>
2025-10-25 23:40:56 +0900

    Title: 사각형 채우기 3, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251025/사각형 채우기 3/rectangle-fill-3.java
+++ 251025/사각형 채우기 3/rectangle-fill-3.java
@@ -6,17 +6,17 @@ public class Main {
     private static long solution(int n) {
         long[] dp = new long[MAX_NUM+1];
         
+        dp[0] = 1;
         dp[1] = 2;
         dp[2] = 7;
 
         for(int i=3; i<=n; i++) {
-            long num = dp[i-1] % MOD;
-            for(int j=0; j<dp[i-2]*2; j++) {
-                // System.out.println(num);
-                dp[i] += num--;
+            dp[i] = ((dp[i-1] * 2L) + (dp[i-2] * 3L)) % MOD;
+            for(int j=i-3; j>=0; j--) {
+                dp[i] = (dp[i] + dp[j] * 2L) % MOD;
             }
         }
-        // System.out.println();
+
         return dp[n];
     }
 
