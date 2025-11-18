7cc7900806ae5a4a652647098107f1202f999b08
Park Do Hoon <139849985+isasgp@users.noreply.github.com>
2025-11-18 23:01:34 +0900

    Title: 최대 증가 부분 수열, Time: 220ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 251118/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    private static final int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        
+        for(int i=0; i<n; i++) {
+            dp[i] = INT_MIN;
+        }
+        dp[0] = 1;
+
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(dp[j] == INT_MIN)
+                    continue;
+                
+                if(arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int result = 0;
+        for(int i=0; i<n; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        System.out.println(result);
+    }
+}
